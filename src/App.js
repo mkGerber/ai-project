088f16fd48dbbabb0f92e9866677137add945e2f
mkGerber <michaelgerber2004@gmail.com>
2024-09-20 14:38:13 -0400

    Begin of AI project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(0);
+  const [type, setType] = useState("all");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>Find a Car</h1>
       </header>
-    </div>
+
+      <main>
+        <section className="infoInput">
+          <h2>Search</h2>
+          <form>
+            <label htmlFor="min">Min Price</label>
+            <input type="number" id="min" name="min" onChange={(e) => setMinPrice(e.target.value)}/>
+
+            <label htmlFor="max">Max Price</label>
+            <input type="number" id="max" name="max" onChange={(e) => setMaxPrice(e.target.value)}/>
+
+            <label htmlFor="type">Type</label>
+            <select id="type" name="type">
+              <option value="all">All</option>
+              <option value="sedan">Sedan</option>
+              <option value="suv">SUV</option>
+              <option value="truck">Truck</option>
+              <option value="coupe">Coupe</option>
+              <option value="convertible">Convertible</option>
+              <option value="hatchback">Hatchback</option>
+              <option value="wagon">Wagon</option>
+              <option value="van">Van</option>  
+            </select>
+
+
+            <button>Search</button>
+          </form>
+        </section>
+      </main>
+    </>
   );
 }
 
